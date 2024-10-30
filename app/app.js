3254932b884fb5e20df269a7d0a7b5699dd46511
Yaroslava Nahorna <yasya74800@gmail.com>
2024-10-30 11:01:28 +0200

    Splash screen: add image and caption

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Splash from ".";
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Splash />
+        </SafeAreaView>
+    )
+}
+export default App;
